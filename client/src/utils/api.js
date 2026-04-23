67af31bde8e98260de52b51a2916cfd2cd63d860
Priyesh Tiwari <priyesh@example.com>
2026-04-23 10:47:44 +0530

    Update frontend API URL for production deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://lost-found-item-management-system.onrender.com/api'
 });
 
 // Add a request interceptor
